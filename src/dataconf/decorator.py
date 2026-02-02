e881c8f4b7673bf7f8add69cf6a622ff994aacbd
Damian D'Souza <dsouzadamian189@gmail.com>
2026-02-02 10:35:56 +0100

    refactor: move types to a separate file

--- src/dataconf/decorator.py
+++ src/dataconf/decorator.py
@@ -1,46 +1,14 @@
 from collections.abc import Callable, Mapping
-from dataclasses import Field, dataclass
 from typing import (
-    TYPE_CHECKING,
-    Any,
-    ClassVar,
-    Protocol,
     TypeVar,
     cast,
     overload,
-    runtime_checkable,
 )
 
 from .state import _registry as registry
-
-if TYPE_CHECKING:
-    from .parser import ConfigParser
-
-
-class FieldDeserializer(Protocol):
-    def __call__(self, value: Any, *, parser: ConfigParser = ...) -> Any: ...
-
-
-type FieldSerializer = Callable[[Any], Any]
-
+from .types import ConfigclassInstance, ConfigSpec, FieldDeserializer, FieldSerializer
 
 _T = TypeVar("_T")
-_T_co = TypeVar("_T_co", covariant=True)
-
-
-@runtime_checkable
-class ConfigclassInstance(Protocol[_T_co]):
-    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
-    __config__: ClassVar[ConfigSpec]
-
-
-@dataclass
-class ConfigSpec:
-    top_level: bool
-    name: str
-    field_mappings: Mapping[str, str]
-    field_deserialzers: Mapping[str, FieldDeserializer]
-    field_serializers: Mapping[str, FieldSerializer]
 
 
 def _apply_config[_T](  # noqa: UP049
--- src/dataconf/state.py
+++ src/dataconf/state.py
@@ -1,7 +1,8 @@
 from typing import TYPE_CHECKING
+
 from .registry import _Registry
 
 if TYPE_CHECKING:
-    from .decorator import ConfigclassInstance
+    from .types import ConfigclassInstance
 
 _registry: _Registry[str, type[ConfigclassInstance]] = _Registry()
