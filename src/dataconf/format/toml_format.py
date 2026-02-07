a3948ca005b3e39c2abb65d75ef4976edc7f91b1
Damian D'Souza <dsouzadamian189@gmail.com>
2026-02-07 17:42:20 +0100

    refactor: add none_value to TOMLFormat constructor

--- src/dataconf/format/toml_format.py
+++ src/dataconf/format/toml_format.py
@@ -5,11 +5,20 @@ import rtoml
 
 
 class TOMLFormat:
+    def __init__(self, none_value: str | None = "null") -> None:
+        """
+        Read and write TOML configuration files.
+        Args:
+            none_value: controls how `None` values are serialized.
+                `none_value=None` means `None` values are ignored.
+        """
+        self.none_value = none_value
+
     def read(self, path: Path) -> dict[str, dict[str, Any]] | None:
         if not path.exists():
             return None
 
-        return rtoml.load(path, none_value="null")
+        return rtoml.load(path, none_value=self.none_value)
 
     def write(self, path: Path, data: dict[str, dict[str, Any]]) -> None:
-        rtoml.dump(data, path, none_value="null")
+        rtoml.dump(data, path, none_value=self.none_value)
