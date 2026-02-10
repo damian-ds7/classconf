7a254b738c7a0aaa7af4dd20f5501f509f1609bb
Damian D'Souza <dsouzadamian189@gmail.com>
2026-02-10 16:49:14 +0100

    chore: add __all__ to __init__ files

--- src/dataconf/__init__.py
+++ src/dataconf/__init__.py
@@ -1,4 +1,13 @@
-from .exceptions import MultipleTopLevelConfigError, InvalidConfigClassError
 from .decorator import configclass
+from .exceptions import InvalidConfigClassError, MultipleTopLevelConfigError
 from .parser import ConfigParser
 from .types import FieldDeserializer, FieldSerializer
+
+__all__ = [
+    configclass,
+    ConfigParser,
+    FieldDeserializer,
+    FieldSerializer,
+    InvalidConfigClassError,
+    MultipleTopLevelConfigError,
+]
--- src/dataconf/format/__init__.py
+++ src/dataconf/format/__init__.py
@@ -1,3 +1,5 @@
 from .config_format import ConfigFormat
 from .toml_format import TOMLFormat
 from .json_format import JSONFormat
+
+__all__ = [ConfigFormat, TOMLFormat, JSONFormat]
