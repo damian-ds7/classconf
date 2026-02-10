f9fc051bcf672f1a9eb5efa6af5c8d33418b3e79
Damian D'Souza <dsouzadamian189@gmail.com>
2026-02-10 23:05:49 +0100

    refactor: make parser staticmethods into module helpers

--- src/classconf/parser.py
+++ src/classconf/parser.py
@@ -1,8 +1,8 @@
-from collections.abc import Mapping
-from dataclasses import MISSING, dataclass, fields, is_dataclass
 import inspect
-from pathlib import Path
 import types
+from collections.abc import Mapping
+from dataclasses import MISSING, fields, is_dataclass
+from pathlib import Path
 from typing import (
     Any,
     TypeGuard,
@@ -14,13 +14,163 @@ from typing import (
     get_type_hints,
 )
 
+from .exceptions import InvalidConfigClassError, MultipleTopLevelConfigError
 from .format.config_format import ConfigFormat
 from .format.toml_format import TOMLFormat
 from .types import ConfigClass, Deser1, Deser2, FieldDeserializer
 from .utils import is_configclass_type
-from .exceptions import MultipleTopLevelConfigError, InvalidConfigClassError
 
 T = TypeVar("T")
+
+
+def _validate_configs(*configs: type[ConfigClass]) -> list[type[ConfigClass[Any]]]:
+    invalid_configs: list[type] = []
+    toplevel_configs: list[type] = []
+
+    for cls in configs:
+        if not is_configclass_type(cls):
+            invalid_configs.append(cls)
+        elif cls.__config__.top_level:
+            toplevel_configs.append(cls)
+
+    if invalid_configs:
+        invalid = ", ".join(cls.__name__ for cls in invalid_configs)
+        raise InvalidConfigClassError(
+            f"Config classes must use @configclass: {invalid}"
+        )
+
+    if len(toplevel_configs) > 1:
+        toplevel = ", ".join(cls.__name__ for cls in toplevel_configs)
+        raise MultipleTopLevelConfigError(
+            f"Only one top-level config class is allowed; found: {toplevel}"
+        )
+
+    return sorted(
+        configs,
+        key=lambda cls: (
+            0 if cls.__config__.top_level else 1,
+            cls.__config__.name.casefold(),
+        ),
+    )
+
+
+def _create_default_config(
+    config_classes: list[type[ConfigClass[Any]]],
+) -> dict[str, Any]:
+    config_data: dict[str, Any] = {}
+
+    for config_class in config_classes:
+        config_class_data = _get_class_fields(config_class)
+
+        if config_class.__config__.top_level:
+            config_data = config_data | config_class_data
+        else:
+            section_name = config_class.__config__.name
+            config_data[section_name] = config_class_data
+
+    return config_data
+
+
+def _get_class_fields(
+    config_class: type[ConfigClass[Any]],
+) -> dict[str, Any]:
+    class_data: dict[str, Any] = {}
+
+    field_mappings = config_class.__config__.field_mappings
+    field_serializers = config_class.__config__.field_serializers
+    resolved_types = get_type_hints(config_class, include_extras=True)
+
+    for field in fields(config_class):
+        key = field_mappings.get(field.name, field.name)
+        field_type = _unwrap_optional(resolved_types[field.name])
+        value = _get_field_default_value(field, field_type)
+
+        value = _serialize_field_value(value, field.name, field_serializers)
+        class_data[key] = value
+
+    return class_data
+
+
+def _get_field_default_value(field: Any, field_type: Any) -> Any:
+    if field.default is not MISSING:
+        return field.default
+    if is_dataclass(field_type):
+        return _get_class_fields(cast(type[ConfigClass], field_type))
+    if field.default_factory is not MISSING:
+        return field.default_factory()
+    return None
+
+
+def _serialize_field_value(
+    value: Any, field_name: str, field_serializers: Mapping[str, Any]
+) -> Any:
+    if field_name in field_serializers:
+        return field_serializers[field_name](value)
+    if isinstance(value, Path):
+        return str(value)
+    return value
+
+
+def _unwrap_optional(field_type: Any) -> Any:
+    origin = get_origin(field_type)
+    if origin is Union or origin is types.UnionType:
+        type_args = [t for t in get_args(field_type) if t is not type(None)]
+        if type_args:
+            return type_args[0]
+    return field_type
+
+
+def _get_root_configs(
+    configs: list[type[ConfigClass[Any]]],
+) -> list[type[ConfigClass[Any]]]:
+    nested_configs = set[type[ConfigClass]]()
+    for config_class in configs:
+        resolved_types = get_type_hints(config_class, include_extras=True)
+        for field in fields(config_class):
+            field_type = _unwrap_optional(resolved_types[field.name])
+            if is_configclass_type(field_type):
+                nested_configs.add(field_type)
+
+    return [c for c in configs if c not in nested_configs]
+
+
+def _convert_field_value(value: Any, field_type: type, parser: "ConfigParser") -> Any:
+    if value is None:
+        return None
+    if is_dataclass(field_type):
+        return _parse_config(cast(type[ConfigClass], field_type), value, parser)
+    elif isinstance(field_type, type) and not isinstance(value, field_type):
+        return field_type(value)
+    return value
+
+
+def _parse_config(
+    config_class: type[ConfigClass[T]],
+    section_data: dict[str, Any],
+    parser: "ConfigParser",
+) -> T:
+    field_mappings = config_class.__config__.field_mappings
+    field_deserializers = config_class.__config__.field_deserialzers
+    kwargs = {}
+
+    resolved_types = get_type_hints(config_class, include_extras=True)
+    for field in fields(config_class):
+        key = field_mappings.get(field.name, field.name)
+
+        if key not in section_data:
+            raise KeyError(f"Missing config key '{key}' for '{config_class.__name__}'")
+
+        value = section_data[key]
+
+        if field.name in field_deserializers:
+            value = apply_deserializer(field_deserializers[field.name], value, parser)
+        else:
+            field_type = _unwrap_optional(resolved_types[field.name])
+            value = _convert_field_value(value, field_type, parser)
+
+        kwargs[field.name] = value
+
+    return cast(T, config_class(**kwargs))
 
 
 def wants_parser(d: FieldDeserializer) -> TypeGuard[Deser2]:
@@ -72,163 +222,16 @@ class ConfigParser:
         self._config_path = config_path
         self._format = format
 
-        self._configs = self._validate_configs(*configs)
+        self._configs = _validate_configs(*configs)
         self._config = self._read_config()
-
-    @staticmethod
-    def _validate_configs(*configs: type[ConfigClass]) -> list[type[ConfigClass[Any]]]:
-        invalid_configs: list[type] = []
-        toplevel_configs: list[type] = []
-
-        for cls in configs:
-            if not is_configclass_type(cls):
-                invalid_configs.append(cls)
-            elif cls.__config__.top_level:
-                toplevel_configs.append(cls)
-
-        if invalid_configs:
-            invalid = ", ".join(cls.__name__ for cls in invalid_configs)
-            raise InvalidConfigClassError(
-                f"Config classes must use @configclass: {invalid}"
-            )
-
-        if len(toplevel_configs) > 1:
-            toplevel = ", ".join(cls.__name__ for cls in toplevel_configs)
-            raise MultipleTopLevelConfigError(
-                f"Only one top-level config class is allowed; found: {toplevel}"
-            )
-
-        return sorted(
-            configs,
-            key=lambda cls: (
-                0 if cls.__config__.top_level else 1,
-                cls.__config__.name.casefold(),
-            ),
-        )
 
     def _read_config(self) -> dict[str, Any]:
         config_data = self._format.read(self._config_path)
         if config_data is None:
-            config_data = self._create_default_config(self._get_root_configs())
+            config_data = _create_default_config(_get_root_configs(self._configs))
             self._format.write(self._config_path, config_data)
 
         return config_data
-
-    @staticmethod
-    def _create_default_config(
-        config_classes: list[type[ConfigClass[Any]]],
-    ) -> dict[str, Any]:
-        config_data: dict[str, Any] = {}
-
-        for config_class in config_classes:
-            config_class_data = ConfigParser._get_class_fields(config_class)
-
-            if config_class.__config__.top_level:
-                config_data = config_data | config_class_data
-            else:
-                section_name = config_class.__config__.name
-                config_data[section_name] = config_class_data
-
-        return config_data
-
-    @staticmethod
-    def _get_class_fields(
-        config_class: type[ConfigClass[Any]],
-    ) -> dict[str, Any]:
-        class_data: dict[str, Any] = {}
-
-        field_mappings = config_class.__config__.field_mappings
-        field_serializers = config_class.__config__.field_serializers
-        resolved_types = get_type_hints(config_class, include_extras=True)
-
-        for field in fields(config_class):
-            key = field_mappings.get(field.name, field.name)
-            field_type = ConfigParser._unwrap_optional(resolved_types[field.name])
-            value = ConfigParser._get_field_default_value(field, field_type)
-
-            value = ConfigParser._serialize_field_value(
-                value, field.name, field_serializers
-            )
-            class_data[key] = value
-
-        return class_data
-
-    @staticmethod
-    def _get_field_default_value(field: Any, field_type: Any) -> Any:
-        if field.default is not MISSING:
-            return field.default
-        elif is_dataclass(field_type):
-            return ConfigParser._get_class_fields(cast(type[ConfigClass], field_type))
-        elif field.default_factory is not MISSING:
-            return field.default_factory()
-        return None
-
-    @staticmethod
-    def _serialize_field_value(
-        value: Any, field_name: str, field_serializers: Mapping[str, Any]
-    ) -> Any:
-        if field_name in field_serializers:
-            return field_serializers[field_name](value)
-        elif isinstance(value, Path):
-            return str(value)
-        return value
-
-    @staticmethod
-    def _unwrap_optional(field_type: Any) -> Any:
-        origin = get_origin(field_type)
-        if origin is Union or origin is types.UnionType:
-            type_args = [t for t in get_args(field_type) if t is not type(None)]
-            if type_args:
-                return type_args[0]
-        return field_type
-
-    def _get_root_configs(self) -> list[type[ConfigClass[Any]]]:
-        nested_configs = set[type[ConfigClass]]()
-        for config_class in self._configs:
-            resolved_types = get_type_hints(config_class, include_extras=True)
-            for field in fields(config_class):
-                field_type = ConfigParser._unwrap_optional(resolved_types[field.name])
-                if is_configclass_type(field_type):
-                    nested_configs.add(field_type)
-
-        return [c for c in self._configs if c not in nested_configs]
-
-    def _convert_field_value(self, value: Any, field_type: type) -> Any:
-        if value is None:
-            return None
-        if is_dataclass(field_type):
-            return self._parse_config(cast(type[ConfigClass], field_type), value)
-        elif isinstance(field_type, type) and not isinstance(value, field_type):
-            return field_type(value)
-        return value
-
-    def _parse_config(
-        self, config_class: type[ConfigClass[T]], section_data: dict[str, Any]
-    ) -> T:
-        field_mappings = config_class.__config__.field_mappings
-        field_deserializers = config_class.__config__.field_deserialzers
-        kwargs = {}
-
-        resolved_types = get_type_hints(config_class, include_extras=True)
-        for field in fields(config_class):
-            key = field_mappings.get(field.name, field.name)
-
-            if key not in section_data:
-                raise KeyError(
-                    f"Missing config key '{key}' for '{config_class.__name__}'"
-                )
-
-            value = section_data[key]
-
-            if field.name in field_deserializers:
-                value = apply_deserializer(field_deserializers[field.name], value, self)
-            else:
-                field_type = ConfigParser._unwrap_optional(resolved_types[field.name])
-                value = self._convert_field_value(value, field_type)
-
-            kwargs[field.name] = value
-
-        return cast(T, config_class(**kwargs))
 
     @staticmethod
     def generate_config(
@@ -252,8 +255,8 @@ class ConfigParser:
         if format is None:
             format = TOMLFormat()
 
-        validated_types = ConfigParser._validate_configs(*types.keys())
-        config = ConfigParser._create_default_config(validated_types)
+        validated_types = _validate_configs(*types.keys())
+        config = _create_default_config(validated_types)
 
         for t in validated_types:
             curr_dict: dict[str, Any]
@@ -270,15 +273,13 @@ class ConfigParser:
                 key = field_mappings.get(field.name, field.name)
                 value = getattr(types[t], field.name)
 
-                value = ConfigParser._serialize_field_value(
-                    value, field.name, field_serializers
-                )
+                value = _serialize_field_value(value, field.name, field_serializers)
                 curr_dict[key] = value
 
         format.write(path, config)
 
     def add(self, *configs: type[Any]) -> None:
-        self._configs = self._validate_configs(*configs, *self._configs)
+        self._configs = _validate_configs(*configs, *self._configs)
 
     def get(self, config_class: type[T]) -> T:
         """
@@ -294,11 +295,11 @@ class ConfigParser:
 
         if cast_class.__config__.top_level:
             config_data = self._config
-            return self._parse_config(cast_class, config_data)
+            return _parse_config(cast_class, config_data, self)
 
         section_name = cast_class.__config__.name
         if section_name not in self._config:
             raise KeyError(f"Missing {section_name} configuration from config file")
 
         config_data = self._config[section_name]
-        return self._parse_config(cast_class, config_data)
+        return _parse_config(cast_class, config_data, self)
